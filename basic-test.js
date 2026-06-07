da68fc65ab54788c6c3a2a6de11a17d246f2c205
ShaminderGit <shaminder.qa@gmail.com>
2026-06-06 21:11:44 -0600

    Add basic k6 test - working

---
+++ basic-test.js
@@ -0,0 +1,9 @@
+import http from 'k6/http';
+import { check } from 'k6';
+
+export default function () {
+  let res = http.get('https://httpbin.org/get');
+  check(res, {
+    'status is 200': (r) => r.status === 200,
+  });
+}
